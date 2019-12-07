5ba326586acd58c00f56a8f68f24deb95eec2355
rob <rob@codebox.org.uk>
2019-12-07 12:59:25 +0000

    refactor

--- src/board.py
+++ src/board.py
@@ -6,13 +6,6 @@ class Board:
         self.rows = rows
         self.board = [[Board.EMPTY_CELL for c in range(cols)] for r in range(rows)]
 
-
-    def set(self, col, row, value):
-        if col < 0 or col >= self.cols or row < 0 or row >= self.rows:
-            raise ValueError('Bad coords in set({},{}) call'.format(col, row))
-
-        self.board[row][col] = value
-
     def drop(self, col, value):
         col_values = list(map(lambda r: r[col], self.board))
         index_of_highest_empty_cell = len(self.board)
@@ -25,18 +18,20 @@ class Board:
                 break
 
         if index_of_highest_empty_cell < len(self.board):
-            self.set(col, index_of_highest_empty_cell, value)
+            self._set(col, index_of_highest_empty_cell, value)
         else:
             raise ValueError('column {} is full'.format(col))
+
+    def _set(self, col, row, value):
+        if col < 0 or col >= self.cols or row < 0 or row >= self.rows:
+            raise ValueError('Bad coords in set({},{}) call'.format(col, row))
+
+        self.board[row][col] = value
 
     def __str__(self):
         return '\n'.join(map(lambda r : ' '.join(map(str,r)), reversed(self.board)))
 
-'''
 b=Board(6,4)
-b.set(1,0,'X')
-print(b)
-print()
 b.drop(1,'X')
 print(b)
 print()
@@ -49,4 +44,6 @@ print(b)
 b.drop(1,'X')
 print()
 print(b)
-'''
+b.drop(1,'X')
+print()
+print(b)

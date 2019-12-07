e21ae8d48e32e3fe26a979faafb8c8a68aea2311
rob <rob@codebox.org.uk>
2019-12-07 12:57:08 +0000

    added board.drop() method

--- src/board.py
+++ src/board.py
@@ -1,22 +1,52 @@
 class Board:
+    EMPTY_CELL = '.'
+
     def __init__(self, cols, rows):
         self.cols = cols
         self.rows = rows
-        self.board = [['.' for c in range(cols)] for r in range(rows)]
+        self.board = [[Board.EMPTY_CELL for c in range(cols)] for r in range(rows)]
+
 
     def set(self, col, row, value):
         if col < 0 or col >= self.cols or row < 0 or row >= self.rows:
             raise ValueError('Bad coords in set({},{}) call'.format(col, row))
 
-        self.board[self.rows-row-1][col] = value
+        self.board[row][col] = value
+
+    def drop(self, col, value):
+        col_values = list(map(lambda r: r[col], self.board))
+        index_of_highest_empty_cell = len(self.board)
+        while True:
+            if col_values[index_of_highest_empty_cell - 1] == Board.EMPTY_CELL:
+                index_of_highest_empty_cell -= 1
+                if index_of_highest_empty_cell == 0:
+                    break
+            else:
+                break
+
+        if index_of_highest_empty_cell < len(self.board):
+            self.set(col, index_of_highest_empty_cell, value)
+        else:
+            raise ValueError('column {} is full'.format(col))
 
     def __str__(self):
-        return '\n'.join(map(lambda r : ' '.join(map(str,r)), self.board))
+        return '\n'.join(map(lambda r : ' '.join(map(str,r)), reversed(self.board)))
 
 '''
 b=Board(6,4)
-b.set(0,0,'X')
-b.set(1,1,'X')
-b.set(5,3,'O')
+b.set(1,0,'X')
+print(b)
+print()
+b.drop(1,'X')
+print(b)
+print()
+b.drop(1,'X')
+print(b)
+print()
+b.drop(1,'X')
+print()
+print(b)
+b.drop(1,'X')
+print()
 print(b)
 '''

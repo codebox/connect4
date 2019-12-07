0fdf684b88a30615ca1b45e532033ad19b5fc86c
rob <rob@codebox.org.uk>
2019-12-07 19:05:15 +0000

    check for lines when adding new discs

--- src/board.py
+++ src/board.py
@@ -1,9 +1,10 @@
 class Board:
     EMPTY_CELL = '.'
 
-    def __init__(self, cols, rows):
+    def __init__(self, cols, rows, line_length):
         self.cols = cols
         self.rows = rows
+        self.line_length = line_length
         self.board = [[Board.EMPTY_CELL for c in range(cols)] for r in range(rows)]
 
     def drop(self, col, value):
@@ -19,31 +20,59 @@ class Board:
 
         if index_of_highest_empty_cell < len(self.board):
             self._set(col, index_of_highest_empty_cell, value)
+            return self._find_lines(col, index_of_highest_empty_cell)
+
         else:
             raise ValueError('column {} is full'.format(col))
 
     def _set(self, col, row, value):
-        if col < 0 or col >= self.cols or row < 0 or row >= self.rows:
-            raise ValueError('Bad coords in set({},{}) call'.format(col, row))
-
+        self._check_coords(col, row)
         self.board[row][col] = value
+
+    def _get(self, col, row):
+        self._check_coords(col, row)
+        return self.board[row][col]
+
+    def _check_coords(self, col, row):
+        if col < 0 or col >= self.cols or row < 0 or row >= self.rows:
+            raise ValueError('Bad coords col={} row={}'.format(col, row))
+
+    def _find_lines(self, col, row):
+        u_count = self._count_in_direction(col, row, -1, 0)
+        d_count = self._count_in_direction(col, row, 1, 0)
+        l_count = self._count_in_direction(col, row, 0, -1)
+        r_count = self._count_in_direction(col, row, 0, 1)
+        ur_count = self._count_in_direction(col, row, -1, 1)
+        ul_count = self._count_in_direction(col, row, -1, -1)
+        dr_count = self._count_in_direction(col, row, 1, 1)
+        dl_count = self._count_in_direction(col, row, 1, -1)
+
+        return list(filter(lambda l: l >= self.line_length, [u_count+d_count-1, r_count+l_count-1, ur_count+dl_count-1, ul_count+dr_count-1]))
+
+    def _count_in_direction(self, init_col, init_row, row_delta, col_delta):
+        col = init_col
+        row = init_row
+        count = 0
+        value = self._get(col, row)
+
+        try:
+            while self._get(col, row) == value:
+                count += 1
+                col += col_delta
+                row += row_delta
+
+        finally:
+            return count
 
     def __str__(self):
         return '\n'.join(map(lambda r : ' '.join(map(str,r)), reversed(self.board)))
 
-b=Board(6,4)
-b.drop(1,'X')
-print(b)
-print()
-b.drop(1,'X')
-print(b)
-print()
-b.drop(1,'X')
-print()
-print(b)
-b.drop(1,'X')
-print()
-print(b)
-b.drop(1,'X')
-print()
-print(b)
+'''
+b=Board(6,4,3)
+print(b.drop(1,'X'))
+print(b.drop(2,'O'))
+print(b.drop(2,'X'))
+print(b.drop(3,'O'))
+print(b.drop(3,'O'))
+print(b.drop(3,'X'))
+'''

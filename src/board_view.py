de7d60e44244b538aac3aa6e628957f69b3debb2
Rob Dawson <rob@dwp>
2019-12-16 15:42:52 +0000

    wip

--- src/board_view.py
+++ src/board_view.py
@@ -2,5 +2,8 @@ class BoardView:
     def __init__(self, board_contents, my_value, empty_value):
         self.board = list(map(lambda row: map(lambda v: 1 if v == my_value else 0 if v == empty_value else -1, row), board_contents))
 
+    def to_id(self):
+		return ','.join(map(lambda r : ''.join(map(str,r)), reversed(self.board)))
+
     def __str__(self):
         return '\n'.join(map(lambda r : ' '.join(map(str,r)), reversed(self.board)))
---
+++ src/mcts/tree.py
@@ -0,0 +1,6 @@
+class Tree:
+	def __init__(self):
+		pass
+
+		
+
